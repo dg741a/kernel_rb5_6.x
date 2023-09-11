790190890496e13ae359424d1eca44dc10c44036
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-09-11 18:27:55 +0300

    fixup! drm/msm/a6xx: Add skeleton A7xx support
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/adreno/a6xx_gmu.c
+++ drivers/gpu/drm/msm/adreno/a6xx_gmu.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2017-2019 The Linux Foundation. All rights reserved. */
 
+#include <linux/bitfield.h>
 #include <linux/clk.h>
 #include <linux/interconnect.h>
 #include <linux/of_platform.h>
